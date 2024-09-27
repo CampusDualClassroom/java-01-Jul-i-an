b9255d383c3e9e167818f86067cab7e711ea51d2
Julian Ramiro Sanchez <julian.ramiro@gmail.com>
2024-09-27 09:29:02 +0200

    Ejercicio realizado Cambio 2

--- src/main/java/com/campusdual/classroom/Exercise01.java
+++ src/main/java/com/campusdual/classroom/Exercise01.java
@@ -4,5 +4,11 @@ public class Exercise01 {
 
     public static void main(String[] args) {
         //TODO → Write: System.out.println("Hello, World!");
+        //Esto funciona o no?
+
+        System.out.println("Hello, World!");
+
+
+
     }
 }
